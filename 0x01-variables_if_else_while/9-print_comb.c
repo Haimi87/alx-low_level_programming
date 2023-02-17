0d0cb62f3c44a65cd4d00b23c774abb8ae892d52
Haimi87 <haimiadmas@gmail.com>
2023-02-17 13:04:27 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entery point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+	return (0);
+}
