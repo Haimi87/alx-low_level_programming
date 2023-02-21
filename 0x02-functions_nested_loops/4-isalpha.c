4805a009ead78393d690d2cf976bf2182925dd24
Haimi87 <haimiadmas@gmail.com>
2023-02-21 10:59:42 -0800

    task 222

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,7 @@
 #include "main.h"
 
 /**
- * _isalpha - Checks if a character is alphabetic.
+ * Test_isalpha - Checks if a character is alphabetic.
  * @c: The character to be checked.
  *
  * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
